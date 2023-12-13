1244f801af7a193f855bbf5837bcb86e7a70a875
Keren Duchan <keren.duchan@gmail.com>
2023-12-13 19:26:40 +0200

    header row for user list and bug list

--- frontend/src/cmps/bug/BugList.jsx
+++ frontend/src/cmps/bug/BugList.jsx
@@ -2,12 +2,22 @@ import { BugPreview } from './BugPreview'
 
 export function BugList({ bugs, onRemoveBug }) {
     return (
-        <ul className="bug-list">
-            {bugs.map((bug) => (
-                <li key={bug._id}>
-                    <BugPreview bug={bug} onRemoveBug={onRemoveBug} />
-                </li>
-            ))}
-        </ul>
+        <div className="bug-list">
+            <div className="header">
+                <div className="title">Severity</div>
+                <div className="title">Created at</div>
+                <div className="title">Title</div>
+                <div className="title">Labels</div>
+                <div className="title">Description</div>
+            </div>
+
+            <ul>
+                {bugs.map((bug) => (
+                    <li key={bug._id}>
+                        <BugPreview bug={bug} onRemoveBug={onRemoveBug} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
--- frontend/src/cmps/user/UserList.jsx
+++ frontend/src/cmps/user/UserList.jsx
@@ -2,12 +2,19 @@ import { UserPreview } from './UserPreview'
 
 export function UserList({ users, onRemoveUser }) {
     return (
-        <ul className="user-list">
-            {users.map((user) => (
-                <li key={user._id}>
-                    <UserPreview user={user} onRemoveUser={onRemoveUser} />
-                </li>
-            ))}
-        </ul>
+        <div className="user-list">
+            <div className="header">
+                <div className="title">Full name</div>
+                <div className="title">Username</div>
+                <div className="title">Score</div>
+            </div>
+            <ul>
+                {users.map((user) => (
+                    <li key={user._id}>
+                        <UserPreview user={user} onRemoveUser={onRemoveUser} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
