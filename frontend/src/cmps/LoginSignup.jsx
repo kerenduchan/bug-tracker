880a6346a6abf8e02d5bb0d1985a07d9a49c33aa
Keren Duchan <keren.duchan@gmail.com>
2023-12-14 21:38:54 +0200

    wip login/signup page

--- frontend/src/cmps/LoginSignup.jsx
+++ frontend/src/cmps/LoginSignup.jsx
@@ -1,3 +1,58 @@
+import { useForm } from '../customHooks/useForm'
+
 export function LoginSignup({ isLogin }) {
-    return <div className="login-signup">{isLogin ? 'login' : 'signup'}</div>
+    const [draft, handleChange] = useForm(getInitialDraft())
+
+    function onSubmit(e) {
+        e.preventDefault()
+    }
+
+    function getInitialDraft() {
+        let initialDraft = { username: '', password: '' }
+        if (!isLogin) {
+            initialDraft.fullname = ''
+        }
+        return initialDraft
+    }
+
+    return (
+        <div className="login-signup">
+            <h1>{isLogin ? 'Log in' : 'Sign up'}</h1>
+
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Username"
+                    onChange={handleChange}
+                    value={draft.username}
+                />
+
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    value={draft.password}
+                />
+
+                {!isLogin && (
+                    <input
+                        type="text"
+                        id="fullname"
+                        name="fullname"
+                        placeholder="Full Name"
+                        onChange={handleChange}
+                        value={draft.fullname}
+                    />
+                )}
+
+                <button className="btn-primary">
+                    {isLogin ? 'Log in' : 'Sign up'}
+                </button>
+            </form>
+        </div>
+    )
 }
