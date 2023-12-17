57dc81cc353ee878bb186b567637f9c74638a70e
Keren Duchan <keren.duchan@gmail.com>
2023-12-17 16:44:04 +0200

    scss

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export function Home() {
     return (
-        <div className="home main-layout">
+        <div className="home main-padding-x">
             <h1>Welcome</h1>
             <p>Welcome to the bug tracker app!</p>
         </div>
