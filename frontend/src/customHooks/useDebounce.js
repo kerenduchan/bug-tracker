95cdc454c639f14a30164b4a7d234ea9330e6ebb
Keren Duchan <keren.duchan@gmail.com>
2023-12-17 12:20:34 +0200

    smaller debounce timeout

--- frontend/src/customHooks/useDebounce.js
+++ frontend/src/customHooks/useDebounce.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-export function useDebounce(value, delay = 500) {
+export function useDebounce(value, delay = 300) {
     const [debouncedValue, setDebouncedValue] = useState(value)
 
     useEffect(() => {
