fbdc27b69d90fa35bff141d4db154fca5504307d
Keren Duchan <keren.duchan@gmail.com>
2023-12-17 18:56:22 +0200

    styling for bug preview

--- frontend/src/cmps/bug/BugList.jsx
+++ frontend/src/cmps/bug/BugList.jsx
@@ -5,11 +5,11 @@ export function BugList({ bugs, onRemoveBug }) {
         <div className="bug-list">
             <div className="header">
                 <div className="title">Severity</div>
-                <div className="title">Created at</div>
-                <div className="title">Created by</div>
                 <div className="title">Title</div>
-                <div className="title">Labels</div>
                 <div className="title">Description</div>
+                <div className="title">Labels</div>
+                <div className="title">Created by</div>
+                <div className="title">Created at</div>
             </div>
 
             <ul className="bugs">
