b76bfd07cb8d1b0d875f45bd0f8f4969b98877a7
Keren Duchan <keren.duchan@gmail.com>
2023-12-21 02:47:55 +0200

    remove unused functions

--- backend/services/util.service.js
+++ backend/services/util.service.js
@@ -5,11 +5,6 @@ import { ObjectId } from 'mongodb'
 
 export const utilService = {
     createObjectId,
-    readJsonFile,
-    writeJsonFile,
-    download,
-    httpGet,
-    makeId,
     toNumber,
     validateMandatoryFields,
     extractFields,
@@ -23,69 +18,6 @@ function createObjectId(id) {
     } catch (err) {
         throw `Invalid ID: ${id}`
     }
-}
-
-function readJsonFile(path) {
-    const str = fs.readFileSync(path, 'utf8')
-    const json = JSON.parse(str)
-    return json
-}
-
-function writeJsonFile(path, obj) {
-    return new Promise((resolve, reject) => {
-        const data = JSON.stringify(obj, null, 2)
-        fs.writeFile(path, data, (err) => {
-            if (err) return reject(err)
-            resolve()
-        })
-    })
-}
-
-function download(url, fileName) {
-    return new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(fileName)
-        https.get(url, (content) => {
-            content.pipe(file)
-            file.on('error', reject)
-            file.on('finish', () => {
-                file.close()
-                resolve()
-            })
-        })
-    })
-}
-
-function httpGet(url) {
-    const protocol = url.startsWith('https') ? https : http
-    const options = {
-        method: 'GET',
-    }
-
-    return new Promise((resolve, reject) => {
-        const req = protocol.request(url, options, (res) => {
-            let data = ''
-            res.on('data', (chunk) => {
-                data += chunk
-            })
-            res.on('end', () => {
-                resolve(data)
-            })
-        })
-        req.on('error', (err) => {
-            reject(err)
-        })
-        req.end()
-    })
-}
-
-function makeId(length = 5) {
-    let text = ''
-    const possible =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
-    }
-    return text
 }
 
 function toNumber(s) {
