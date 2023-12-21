d6a8da5e3b8e35acbcf83ef8214960e5d85e0d8b
Keren Duchan <keren.duchan@gmail.com>
2023-12-21 19:06:08 +0200

    remove unused code

--- frontend/src/services/util.service.js
+++ frontend/src/services/util.service.js
@@ -2,8 +2,6 @@ import moment from 'moment'
 
 export const utilService = {
     makeId,
-    makeLorem,
-    getRandomIntInclusive,
     getSortDirOptions,
     saveToStorage,
     loadFromStorage,
@@ -23,55 +21,6 @@ function makeId(length = 6) {
     }
 
     return txt
-}
-
-function makeLorem(size = 100) {
-    var words = [
-        'The sky',
-        'above',
-        'the port',
-        'was',
-        'the color of television',
-        'tuned',
-        'to',
-        'a dead channel',
-        '.',
-        'All',
-        'this happened',
-        'more or less',
-        '.',
-        'I',
-        'had',
-        'the story',
-        'bit by bit',
-        'from various people',
-        'and',
-        'as generally',
-        'happens',
-        'in such cases',
-        'each time',
-        'it',
-        'was',
-        'a different story',
-        '.',
-        'It',
-        'was',
-        'a pleasure',
-        'to',
-        'burn',
-    ]
-    var txt = ''
-    while (size > 0) {
-        size--
-        txt += words[Math.floor(Math.random() * words.length)] + ' '
-    }
-    return txt
-}
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive
 }
 
 function getSortDirOptions() {
