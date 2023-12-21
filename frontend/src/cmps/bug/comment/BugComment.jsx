48ac1c76efe784cb959b10f3671a78e8c027bc4b
Keren Duchan <keren.duchan@gmail.com>
2023-12-21 18:40:02 +0200

    frontend authorization for edit/delete comment

--- frontend/src/cmps/bug/comment/BugComment.jsx
+++ frontend/src/cmps/bug/comment/BugComment.jsx
@@ -1,10 +1,21 @@
+import { useContext } from 'react'
+import { commentService } from '../../../services/comment.service'
 import { utilService } from '../../../services/util.service'
+import { LoginContext } from '../../../contexts/LoginContext'
 
 export function BugComment({ comment, onDelete }) {
+    const { loggedinUser } = useContext(LoginContext)
+
     function onEdit() {
         console.log('on edit')
     }
 
+    function isActionsAllowed() {
+        return commentService.isEditOrDeleteCommentAllowed(
+            loggedinUser,
+            comment
+        )
+    }
     return (
         <div className="bug-comment">
             <div className="created-at">
@@ -14,14 +25,16 @@ export function BugComment({ comment, onDelete }) {
             <div className="txt">
                 <pre>{comment.txt}</pre>
             </div>
-            <div className="bug-comment-actions">
-                <div className="bug-comment-action" onClick={onEdit}>
-                    Edit
+            {isActionsAllowed() && (
+                <div className="bug-comment-actions">
+                    <div className="bug-comment-action" onClick={onEdit}>
+                        Edit
+                    </div>
+                    <div className="bug-comment-action" onClick={onDelete}>
+                        Delete
+                    </div>
                 </div>
-                <div className="bug-comment-action" onClick={onDelete}>
-                    Delete
-                </div>
-            </div>
+            )}
         </div>
     )
 }
--- frontend/src/services/comment.service.js
+++ frontend/src/services/comment.service.js
@@ -6,9 +6,19 @@ const service = commentAxiosService
 export const commentService = {
     ...service,
     isCreateCommentAllowed,
+    isEditOrDeleteCommentAllowed,
 }
 
 // a user is allowed to create a comment only if they are logged in
 function isCreateCommentAllowed(loggedinUser) {
     return loggedinUser
+}
+
+// a user is allowed to edit/delete a comment only if they are logged in and
+// they are an admin or they are the comment creator
+function isEditOrDeleteCommentAllowed(loggedinUser, comment) {
+    return (
+        loggedinUser &&
+        (loggedinUser.isAdmin || loggedinUser._id === comment.creator._id)
+    )
 }
