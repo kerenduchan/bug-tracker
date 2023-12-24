7d82a2e51d0f936a34ce7a944a69624218a8fcd4
Keren Duchan <keren.duchan@gmail.com>
2023-12-24 17:08:56 +0200

    debug print in build.sh for render.com

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,12 @@
 # Build frontend (into backend/public dir)
+echo "Building frontend..."
 cd ../frontend
+pwd
 npm i
 npm run build
 
 # Build backend
+echo "Building backend..."
 cd ../backend
+pwd
 npm i
