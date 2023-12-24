52f423ef17b18058def454358acb0bf4deb9da39
Keren Duchan <keren.duchan@gmail.com>
2023-12-24 17:32:53 +0200

    workaround for render.com

--- backend/build.sh
+++ backend/build.sh
@@ -1,16 +1,10 @@
 # Build frontend (into backend/public dir)
 echo "Building frontend..."
 cd ../frontend
-pwd
-npm install --legacy-peer-deps
-echo "ls node_modules/"
-ls node_modules/
-echo "ls node_modules/.bin"
-ls node_modules/.bin
+npm i
 npm run build
 
 # Build backend
 echo "Building backend..."
 cd ../backend
-pwd
-npm install
+npm i
